8c67615469e3085e9f92630fa9aa5efbf7768654
Michael Macewich <maceww6438@gmail.com>
2023-06-13 11:50:32 -0500

    On branch main  Changes to be committed: 	modified:   planes.py

--- planes.py
+++ planes.py
@@ -19,9 +19,31 @@ category=17
 
 import requests
 
+categoryTable = {
+	0 : "No information at all",
+	1 : "No ADS-B Emitter Category Information",
+	2 : "Light (< 15500 lbs)",
+	3 : "Small (15500 to 75000 lbs)",
+	4 : "Large (75000 to 300000 lbs)",
+	5 : "High Vortex Large (aircraft such as B-757)",
+	6 : "Heavy (> 300000 lbs)",
+	7 : "High Performance (> 5g acceleration and 400 kts)",
+	8 : "Rotorcraft",
+	9 : "Glider / sailplane",
+	10 : "Lighter-than-air",
+	11 : "Parachutist / Skydiver",
+	12 : "Ultralight / hang-glider / paraglider",
+	13 : "Reserved",
+	14 : "Unmanned Aerial Vehicle",
+	15 : "Space / Trans-atmospheric vehicle",
+	16 : "Surface Vehicle – Emergency Vehicle",
+	17 : "Surface Vehicle – Service Vehicle",
+	18 : "Point Obstacle (includes tethered balloons)",
+	19 : "Cluster Obstacle",
+	20 : "Line Obstacle",
+}
 
 # Make the REST API request
-url = "https://opensky-network.org/api/states/all"
 url = "https://opensky-network.org/api/states/all?lamin=41.6&lomin=-88.5&lamax=42.0&lomax=-87.6&extended=1"
 
 response = requests.get(url)
